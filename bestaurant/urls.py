53d146b02b68e1790665ff34983104b67ce3c25b
Emilie Ewing <2653131e@student.gla.ac.uk>
2023-03-03 14:25:19 +0000

    bug fix on includes

--- bestaurant/urls.py
+++ bestaurant/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',  include('accounts.urls')),
-          #include('registration.backends.simple.urls'),
+    include('registration.backends.simple.urls'),
     # path('search', include('search.urls')), add in when has valid patterns
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
