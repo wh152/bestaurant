4c14a4089993af12f5a20326ff4e7afd5c7dde23
Ben Morrison <2663399M@student.gla.ac.uk>
2023-03-09 21:47:25 +0000

    other saved files

--- other/urls.py
+++ other/urls.py
@@ -6,6 +6,7 @@ app_name = 'other'
 urlpatterns = [
     # path('advertise/', views.createAdvert, name='createAdvert'),
     path('managerestaurants/', views.viewOwnRestaurants, name='viewOwnRestaurants'),
+    path('managerestaurants/add', views.addRestaurant, name='addRestaurant'),
     path('user/', views.viewAllUsers, name='viewAllUsers'),
     path('user/<str:username>', views.viewOneUser, name='viewOneUser'),
     path('advertise/', views.advertise, name='advertise'),
