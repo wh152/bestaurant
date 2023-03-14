f0416ad2d4c42cfa92d6fa6428e91f2467c5dd92
EmmaNeillie <2617836n@student.gla.ac.uk>
2023-03-14 19:01:14 +0000

    Bug Fix

--- other/models.py
+++ other/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from accounts.models import Restaurant
 
-class Review(models.Model):
-    restaurantID = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
 
 #Base review model for foreign key field
