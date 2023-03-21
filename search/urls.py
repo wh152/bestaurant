1c33c95ab019c87bf4ebd2ff445ae021100551cb
EmmaNeillie <2617836n@student.gla.ac.uk>
2023-03-21 21:34:40 +0000

    Update urls.py

--- search/urls.py
+++ search/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("", views.search_results, name='search_results'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
-    path('', views.index),
+    #path('', views.index),
     path('',views.most_recently_reviewed, name='most_recently_reviewed'),
     path('',views.most_reviewed, name='most_reviewed'),
     path('',views.recently_added, name='recently_added'),
