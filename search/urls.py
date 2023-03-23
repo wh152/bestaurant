afd3ddc9d6602a10e3b8c21f1a2a0add7c326937
William Hunt <2620286h@student.gla.ac.uk>
2023-03-23 10:12:41 +0000

    filtering urls have dashes not underscores

--- search/urls.py
+++ search/urls.py
@@ -7,9 +7,9 @@ app_name = 'search'
 urlpatterns = [
     path('', views.search_results, name='search_results'),
     # the 3 filters for all advertisements
-    path('most_reviewed/', views.most_reviewed, name='most_reviewed'),
-    path('most_recently_reviewed/', views.most_recently_reviewed, name='most_recently_reviewed'),
-    path('recently_added/', views.recently_added, name='recently_added'),
+    path('most-reviewed/', views.most_reviewed, name='most_reviewed'),
+    path('most-recently-reviewed/', views.most_recently_reviewed, name='most_recently_reviewed'),
+    path('recently-added/', views.recently_added, name='recently_added'),
     # filtering by category
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
